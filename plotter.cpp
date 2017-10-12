e51bf45b85b9b85a5b6e010ef27ae3425b4c0dcd
stup4r <plassoul@gmail.com>
2017-10-13 01:30:22 +0200

    Licencing

--- plotter.cpp
+++ plotter.cpp
@@ -1,4 +1,16 @@
-﻿#include "plotter.h"
+﻿/*************************************************************************
+ *
+ *  NanoSignal: Signal analysis toolbox.
+ *  Copyright (C) 2017  Petar Stupar
+ *  Collective Copyright Holder.
+ *  Licensed under the GPLv3.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************/
+
+#include "plotter.h"
 
 Plotter::Plotter(CustomPlotZoom* externalPlot)
 {
--- variance.cpp
+++ variance.cpp
@@ -1,3 +1,15 @@
+/*************************************************************************
+ *
+ *  NanoSignal: Signal analysis toolbox.
+ *  Copyright (C) 2017  Petar Stupar
+ *  Collective Copyright Holder.
+ *  Licensed under the GPLv3.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ************************************************************************/
+
 #include "variance.h"
 
 Variance::Variance()
